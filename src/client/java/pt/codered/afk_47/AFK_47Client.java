91a20ccb1620cf80ac51adff6873748351983a5d
Guilherme Proença Araújo <guilhermeproencaaraujo@gmail.com>
2025-12-05 02:34:54 +0000

    Adds baritone and nether pathfinder dependencies
    Adds baritone api as a dependency and a maven repo for nether pathfinder.
    
    Also adds a client command as a sanity check

--- src/client/java/pt/codered/afk_47/AFK_47Client.java
+++ src/client/java/pt/codered/afk_47/AFK_47Client.java
@@ -1,10 +1,23 @@
 package pt.codered.afk_47;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.minecraft.text.Text;
 
 public class AFK_47Client implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+            
+            dispatcher.register(ClientCommandManager.literal("afktest")
+                    .executes(context -> {
+                        context.getSource().sendFeedback(Text.literal("§aAFK_47 Client is working!"));
+                        return 1;
+                    })
+            );
+        });
+    }
 }
